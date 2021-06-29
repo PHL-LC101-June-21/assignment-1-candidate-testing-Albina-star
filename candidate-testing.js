b11adc03e819c409f5e921cfd5bc7e7035254134
Albina <65147059+Albina-star@users.noreply.github.com>
2021-06-29 00:58:20 +0000

    Final version: added one more loop to the function gradeQuiz(candidateAnswers)

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,7 +11,7 @@ let correctAnswer;
 let candidateAnswer;
 let candidateAnswers = [];
 let question;
-let answerGrade = " ";
+let answerGrade = 0;
 let status = " ";
 
 const questions = ["1) Who was the first American woman in space? ", "2)True or false: 5000 meters = 5 kilometers.", "3)(5 + 3)/2 * 10 = ?","4)Given the array [8, Orbit, Trajectory, 45]what entry is at index 2?", "5)What is the minimum crew size for the International Space Station(ISS)?"];
@@ -20,7 +20,9 @@ const correctAnswers = ["Sally Ride", "True", "40", "Trajectory","3"];
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-candidateName = input.question("As a candidate, please enter your name:  ");
+ 
+  candidateName = input.question("As a candidate, please enter your name:  ");
+  console.log ("Candidate Name: ", candidateName);
 }
 
 function askQuestion(i) {
@@ -28,56 +30,46 @@ function askQuestion(i) {
   for (let i =0; i<questions.length; i++){
     candidateAnswers.push(input.question(questions[i]));
   }
+  
 }
-
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 function gradeQuiz(candidateAnswers) {
-    for (let i=0; i < questions.length; i = i + 1){ 
-  if (candidateAnswers[i] == correctAnswers[i]){
-   answerGrade = answerGrade + 1;
-  }
- totalGrade= (((answerGrade) / 5) * 100);
+  for (let i=0; i < questions.length; i = i + 1) { 
+    if (candidateAnswers[i] == correctAnswers[i]){
+      answerGrade = answerGrade + 1;
     }
+    totalGrade= (((answerGrade) / 5) * 100);
+  }
+
     if (totalGrade >= 80){
     quizeStatus = "SUCCESS";
-  } else { quizeStatus = "FAILED";
+    } else {
+    quizeStatus = "FAILED";
   }
-  return quizeStatus;
+
+  
+  for (let i  =0; i < candidateAnswers.length; i++ ) {
+    console.log (questions[i]);
+    console.log ("Your Answer: ", candidateAnswers[i]);
+    console.log ("Correct Answer: ",correctAnswers[i]);
+    console.log ("                                      ");
+  }
+    
+    console.log(`>>> Overall Grade: ${totalGrade}% (${answerGrade} of 5 responses correct) <<<`);
+    console.log (`>>> Status:${quizeStatus} <<<`);
+ 
+  return totalGrade;
  }
  
- function runProgram() {
+function runProgram() {
   
   // TODO 1.1c: Ask for candidate's name //
   askForName();
   askQuestion();
   gradeQuiz(this.candidateAnswers); 
 
-  console.log ("Candidate Name: ", candidateName);
-  console.log (questions[0]);
-  console.log ("Your Answer: ", candidateAnswers[0]);
-  console.log ("Correct Answer: ",correctAnswers[0]);
-  console.log ("                                      ");
-  console.log (questions[1]);
-  console.log ("Your Answer: ", candidateAnswers[1]);
-  console.log ("Correct Answer: ",correctAnswers[1]);
-  console.log ("                                      ");
-  console.log (questions[2]);
-  console.log ("Your Answer: ",candidateAnswers[2]);
-  console.log ("Correct Answer: ",correctAnswers[2]);
-  console.log ("                                      ");
-  console.log (questions[3]);
-  console.log ("Your Answer: ", candidateAnswers[3]);
-  console.log ("Correct Answer: ",correctAnswers[3]);
-  console.log ("                                      ");
-  console.log (questions[4]);
-  console.log ("Your Answer: ", candidateAnswers[4]);
-  console.log ("Correct Answer: ",correctAnswers[4]);
-  console.log ("                                      ");
-  console.log ("                                      ");
-  console.log(`>>> Overall Grade: ${answerGrade}% ${totalGrade} of 5 responses correct) <<<`);
-  console.log (`>>> Status:${quizeStatus} <<<`);
- };
- 
+ }
+
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
 
